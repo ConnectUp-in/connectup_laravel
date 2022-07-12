30d625345b0c58fffaa5fd72588ac7d266e26df5
meinhoonharsh <harshprogrammer782@gmail.com>
2022-07-13 01:17:39 +0530

    Created User Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+            'name' => 'Test User',
+            'email' => 'email@gmail.com',
+            'password' => 'password',
+            'username' => 'testuser',
+            'role' => 0,
+            ],
+            [
+                'name' => 'Harsh Vishwakarma',
+                'email' => 'harshprogrammer782@gmail.com',
+                'password' => 'password',
+                'username' => 'founder',
+                'role' => 111,
+            ]
+    
+    ];
+
+        foreach ($users as $user) {
+            $user =  new User($user);
+            $user->save();
+        }
+
+    }
+}
