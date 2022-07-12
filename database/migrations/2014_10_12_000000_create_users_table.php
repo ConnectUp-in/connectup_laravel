339d1c782d1e9ef14f05b4326600f0e61ba554e1
meinhoonharsh <harshprogrammer782@gmail.com>
2022-07-13 01:17:19 +0530

    Added USername and fixed id in migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->string('id');
             $table->primary('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             // role, active, stream, college, bio, about, graduation_year,  interests,  cover_image
