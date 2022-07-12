b7bf5a1983bb1050175b276669c40b2282fad34e
meinhoonharsh <harshprogrammer782@gmail.com>
2022-07-13 01:26:24 +0530

    Added Fields in Posts Migration

--- database/migrations/2022_07_12_194922_create_posts_table.php
+++ database/migrations/2022_07_12_194922_create_posts_table.php
@@ -14,7 +14,22 @@ return new class extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            // id, user, caption, images, tags, ytlink, type, question,  options, answer, active, shares, likes, dislikes, created_at, updated_at
+            $table->string('id');
+            $table->primary('id');
+            $table->foreignId('user_id');
+            $table->string('caption')->nullable();
+            $table->json('images')->default(json_encode([]));
+            $table->json('tags')->default(json_encode([]));
+            $table->string('ytlink')->nullable();
+            $table->string('type')->default('n');
+            $table->string('question')->nullable();
+            $table->json('options')->default(json_encode([]));
+            $table->string('answer')->nullable();
+            $table->boolean('active')->default(true);
+            $table->json('shares')->default(json_encode([]));
+            $table->json('likes')->default(json_encode([]));
+            $table->json('dislikes')->default(json_encode([]));
             $table->timestamps();
         });
     }
