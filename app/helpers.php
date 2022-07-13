f1e9e3a2160d8cefff4005ca0a842ebe641a2717
meinhoonharsh <harshprogrammer782@gmail.com>
2022-07-13 09:56:52 +0530

    Added getMetaData in helpers

--- app/helpers.php
+++ app/helpers.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Auth;
 
+// Import DOMDocument class
+// use DOMDocument;
+
+
 function makeHyperText($text){
     return preg_replace('!(https://[a-z0-9_./?=&-]+)!i', '<a href="$1">$1</a> ', $text." ");
 }
@@ -10,4 +14,55 @@ function getURLfromText($text){
     preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $text, $match);
     $string = implode(',', $match[0]);
     return $string;
+}
+
+
+function getMetaData($url){
+    $resp = [];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    // Load HTML to DOM Object
+    $dom = new DOMDocument();
+    @$dom->loadHTML($data);
+
+    // Parse DOM to get Title
+    $nodes = $dom->getElementsByTagName('title');
+    $title = '';
+    if($nodes->length > 0){
+        $title = $nodes->item(0)->nodeValue;
+    }
+    // Parse DOM to get Meta Description
+    $metas = $dom->getElementsByTagName('meta');
+    $body = "";
+    $imageURL = "";
+    for ($i = 0; $i < $metas->length; $i ++) {
+        $meta = $metas->item($i);
+        if ($meta->getAttribute('name') == 'Description' || $meta->getAttribute('name') == 'description') {
+            $body = $meta->getAttribute('content');
+        }
+        if ($meta->getAttribute('property') == 'og:image:url') {
+            $imageURL = $meta->getAttribute('content');
+        }elseif($meta->getAttribute('property') == 'og:image'){
+            $imageURL = $meta->getAttribute('content');
+        }
+    }
+
+    $images = $dom->getElementsByTagName('img');
+
+    if(!isset($imageURL) && $images->length > 0){
+        $imageURL = $images->item(0)->getAttribute('src');
+    }
+
+    $output = array(
+        'title' => $title,
+        'image_url' => $imageURL,
+        'url' => $url,
+        'body' => preg_replace('/[^\00-\255]+/u', '', $body)
+    );
+    return $output;
 }
