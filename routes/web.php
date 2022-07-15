b91437f4384382542287a4346692d93992846a67
meinhoonharsh <harshprogrammer782@gmail.com>
2022-07-15 23:42:02 +0530

    Fixed  photo_path_issue

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
 
 
 Route::get('test', function(){
