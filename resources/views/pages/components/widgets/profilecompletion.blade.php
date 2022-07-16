081120e011a8684646380d7b7fe032120aa5306f
meinhoonharsh <harshprogrammer782@gmail.com>
2022-07-16 23:18:15 +0530

    Updated Profile completion widget

--- resources/views/pages/components/widgets/profilecompletion.blade.php
+++ resources/views/pages/components/widgets/profilecompletion.blade.php
@@ -1,51 +1,51 @@
+@if (Auth::check() && profileNotCompleted())
+    <!-- WIDGET BOX -->
+    <div class="widget-box">
+        <!-- PROGRESS ARC SUMMARY -->
+        <div class="progress-arc-summary">
+            <!-- PROGRESS ARC WRAP -->
+            <div class="progress-arc-wrap">
+                <!-- PROGRESS ARC -->
+                <div class="progress-arc">
+                    <canvas id="profile-completion-chart" data-percentage="80"></canvas>
+                </div>
+                <!-- PROGRESS ARC -->
 
-                <!-- WIDGET BOX -->
-                <div class="widget-box">
-                    <!-- PROGRESS ARC SUMMARY -->
-                    <div class="progress-arc-summary">
-                        <!-- PROGRESS ARC WRAP -->
-                        <div class="progress-arc-wrap">
-                            <!-- PROGRESS ARC -->
-                            <div class="progress-arc">
-                                <canvas id="profile-completion-chart" data-percentage="80"></canvas>
-                            </div>
-                            <!-- PROGRESS ARC -->
+                <!-- PROGRESS ARC INFO -->
+                <div class="progress-arc-info">
+                    <!-- PROGRESS ARC TITLE -->
+                    <p class="progress-arc-title">80%</p>
+                    <!-- /PROGRESS ARC TITLE -->
+                </div>
+                <!-- /PROGRESS ARC INFO -->
+            </div>
+            <!-- /PROGRESS ARC WRAP -->
 
-                            <!-- PROGRESS ARC INFO -->
-                            <div class="progress-arc-info">
-                                <!-- PROGRESS ARC TITLE -->
-                                <p class="progress-arc-title">80%</p>
-                                <!-- /PROGRESS ARC TITLE -->
-                            </div>
-                            <!-- /PROGRESS ARC INFO -->
-                        </div>
-                        <!-- /PROGRESS ARC WRAP -->
+            <!-- PROGRESS ARC SUMMARY INFO -->
+            <div class="progress-arc-summary-info">
+                <!-- PROGRESS ARC SUMMARY TITLE -->
+                <p class="progress-arc-summary-title">Profile Completion</p>
+                <!-- /PROGRESS ARC SUMMARY TITLE -->
 
-                        <!-- PROGRESS ARC SUMMARY INFO -->
-                        <div class="progress-arc-summary-info">
-                            <!-- PROGRESS ARC SUMMARY TITLE -->
-                            <p class="progress-arc-summary-title">Profile Completion</p>
-                            <!-- /PROGRESS ARC SUMMARY TITLE -->
+                <!-- PROGRESS ARC SUMMARY TITLE -->
+                <p class="progress-arc-summary-subtitle">{{ '@' . Auth::user()->username }}</p>
+                <!-- /PROGRESS ARC SUMMARY TITLE -->
 
-                            <!-- PROGRESS ARC SUMMARY TITLE -->
-                            <p class="progress-arc-summary-subtitle">{{ '@' . Auth::user()->username }}</p>
-                            <!-- /PROGRESS ARC SUMMARY TITLE -->
-
-                            {{-- <!-- PROGRESS ARC SUMMARY TITLE -->
+                {{-- <!-- PROGRESS ARC SUMMARY TITLE -->
                             <p class="progress-arc-summary-text">Complete your profile by filling profile info fields.</p>
                             <!-- /PROGRESS ARC SUMMARY TITLE --> --}}
-                        </div>
-                        <!-- /PROGRESS ARC SUMMARY INFO -->
-                    </div>
-                    <!-- /PROGRESS ARC SUMMARY -->
+            </div>
+            <!-- /PROGRESS ARC SUMMARY INFO -->
+        </div>
+        <!-- /PROGRESS ARC SUMMARY -->
 
-                    {{-- Button for Profile Completion --}}
-                    <div>
+        {{-- Button for Profile Completion --}}
+        <div>
 
-                        <a href="/profile" class="button primary w-100 mt-3">Complete Your Profile</a>
-                    </div>
+            <a href="/profile" class="button primary w-100 mt-3">Complete Your Profile</a>
+        </div>
 
-                    {{-- <!-- ACHIEVEMENT STATUS LIST -->
+        {{-- <!-- ACHIEVEMENT STATUS LIST -->
                     <div class="achievement-status-list">
                         <!-- ACHIEVEMENT STATUS -->
                         <div class="achievement-status">
@@ -98,5 +98,6 @@
                         <!-- /ACHIEVEMENT STATUS -->
                     </div>
                     <!-- /ACHIEVEMENT STATUS LIST --> --}}
-                </div>
-                <!-- /WIDGET BOX -->
+    </div>
+    <!-- /WIDGET BOX -->
+@endif
