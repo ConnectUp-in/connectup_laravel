6f994541ddcce11edefef65ec2ce64ed5b70bf5a
meinhoonharsh <harshprogrammer782@gmail.com>
2022-07-16 21:13:24 +0530

    Created own import directives

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+//  Import blade
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +26,27 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Blade::directive('import', function ($args) {
+            $args = Blade::stripParentheses($args);
+    
+            $viewBasePath = Blade::getPath();
+            foreach ($this->app['config']['view.paths'] as $path) {
+                if (substr($viewBasePath,0,strlen($path)) === $path) {
+                    $viewBasePath = substr($viewBasePath,strlen($path));
+                    break;
+                }
+            }
+    
+            $viewBasePath = dirname(trim($viewBasePath,'\/'));
+            $args = substr_replace($args, $viewBasePath.'.', 1, 0);
+            return "<?php echo \$__env->make({$args}, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>";
+        });
+
+
+        Blade::directive('widget', function ($args) {
+            $args = Blade::stripParentheses($args);
+            return "<?php echo \$__env->make('pages.components.widgets.'.$args, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>";
+        });
+
     }
 }
