f1afeeaa233fad2af83071c0f2f615af230c3744
meinhoonharsh <harshprogrammer782@gmail.com>
2022-07-16 23:38:12 +0530

    Completed migration and Model for Interest

--- app/Models/Interest.php
+++ app/Models/Interest.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Interest extends Model
 {
     use HasFactory;
+    public $timestamps = false;
 }
--- database/migrations/2022_07_16_175019_create_interests_table.php
+++ database/migrations/2022_07_16_175019_create_interests_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('interests', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
         });
     }
 
