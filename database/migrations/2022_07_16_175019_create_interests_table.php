a66f166efce38ed67aada1de717106d4cb880884
meinhoonharsh <harshprogrammer782@gmail.com>
2022-07-22 22:45:13 +0530

    Added category in interests

--- database/migrations/2022_07_16_175019_create_interests_table.php
+++ database/migrations/2022_07_16_175019_create_interests_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('interests', function (Blueprint $table) {
             $table->id();
+            $table->string('category')->nullable();
             $table->string('name');
         });
     }
