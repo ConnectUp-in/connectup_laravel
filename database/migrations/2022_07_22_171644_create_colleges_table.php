75c38d4874d7098862d6044a7d8188333e299586
meinhoonharsh <harshprogrammer782@gmail.com>
2022-07-22 23:01:05 +0530

    Created College migtrastion

---
+++ database/migrations/2022_07_22_171644_create_colleges_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('colleges', function (Blueprint $table) {
+            $table->id();
+            $table->string('university')->nullable();
+            $table->string('college')->nullable();
+            $table->string('college_type')->nullable();
+            $table->string('state')->nullable();
+            $table->string('district')->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('colleges');
+    }
+};
