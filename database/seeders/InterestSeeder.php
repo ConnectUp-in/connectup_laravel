8bccb4b3b035aa28b6d3229629cd8668ef45e66b
meinhoonharsh <harshprogrammer782@gmail.com>
2022-07-22 22:44:59 +0530

    Updated Interests

--- database/seeders/InterestSeeder.php
+++ database/seeders/InterestSeeder.php
@@ -15,30 +15,105 @@ class InterestSeeder extends Seeder
      */
     public function run()
     {
-            
-        $interests = [
-            'Graphic Designing',
-            'Marketing',
-            'Programming',
-            'Content Writing',
-            'Web Development',
-            'App Development',
-            'Backend Development',
-            'Frontend Development',
-            'Finance & Accounting',
-            'Business Development',
-            'Human Resources',
-            'Sales',
-            'Project Management',
-            'Law & Legality',
-            'Multitasking',
-            'Networking',
-        ];
+            $interests = [
+                'business'=>[
+                    'Entrepreneurship',
+                    'Outsourcing',
+                    'Startups',
+                    'Startup Accelerator',
+                    'Venture Capitalist',
+                    'Startup Coach and Mentor',
+                    'Startup Founder',
+                    'Startup Investor',
+                    'Pitch Deck',
+                    'Pitch Deck Review',
+                    'Networking',
+                ],
+                'technology'=>[
+                    'Full Stack Developement',
+                    'Front End Development',
+                    'Back End Development',
+                    'Mobile Development',
+                    'Web Development',
+                    'Software Development',
+                    'DevOps',
+                    'Data Science',
+                    'Cyber Security',
+                    'Cloud Computing',
+                    'Blockchain',
+                    'Machine Learning',
+                    'Artificial Intelligence',
+                    'IoT',
+                    'VR/AR',
+                    'Robotics',
+                    'RPA',                  
+                ],
+                'selfgrowth'=>[
+                    'Investment',
+                    'Public Speaking',
+                    'Personal Branding',
+                    'Stocks',
+                    'Cryptocurrency',
+                    'Upskilling',
+                    'Career Development',
+                    'Career Coaching',
+                ],
+                'marketing'=>[
+                    'Performance Marketing',
+                    'Marketing',
+                    'SEO',
+                    'SMM',
+                    'SEM',
+                    'Email Marketing',
+                    'Social Media Marketing',
+                    'Content Marketing',
+                    'PR',
+                    'Public Relations',
+                    'Brand Marketing',
+                    'Brand Development',
+                    'Brand Strategy',
+                    'B2B Marketing',
+                    'B2C Marketing',
+                    'Marketing Strategy',
+                    'Market Research',
+                    'Market Analysis',
+                    'Influencer Marketing',
+                ],
+                'creativity'=>[
+                    'Animation',
+                    'Art',
+                    'Design',
+                    'Illustration',
+                    'Photography',
+                    'Video Production',
+                    'Video Editing',
+                    'Video Animation',
+                    'Video Filming',
+                    'Writing',
+                    'Copywriting',
+                    'Translation',
+                    'Web Design',
+                    'Graphic Design',
+                    'Advertising',
+                    'Media Design',
+                    'Packaging',
+                    'Product Design',
+                    'UI/UX Design',
+                ],
+                'legal'=>[
+                    'Law',
+                    'Legal',
+                    'Legal Advice',
+                ]
+         ];
 
-        foreach ($interests as $interest) {
-            Interest::create([
-                'name' => $interest,
-            ]);
+        foreach ($interests as $category => $i) {
+            foreach ($i as $interest) {
+                Interest::create([
+                    'category' => $category,
+                    'name' => $interest
+                ]);
+            }
         }
 
     }
