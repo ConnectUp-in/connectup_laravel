ffbda1cbbceb45a72710ce8b00d01c889f76090c
meinhoonharsh <harshprogrammer782@gmail.com>
2022-07-23 18:38:07 +0530

    Created College Search API

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/search/college/{query}',[SearchController::class,'college']);
