a2f98e6e6e663e3d1f31f4046b45cbe2ea027743
meinhoonharsh <harshprogrammer782@gmail.com>
2022-08-22 23:20:26 +0530

    Added Brickstox in startups

--- database/seeders/StartupSeeder.php
+++ database/seeders/StartupSeeder.php
@@ -18,6 +18,7 @@ class StartupSeeder extends Seeder
     {
         // id, name, bio, about, logo, cover, socials, founded_at, contact_email, hiring_email, founder, category, stage, contact, funded, interests, active, lookingfor, created_at, updated_at
         $founder = User::where('username', 'founder')->first();
+        $rkdy49 = User::where('username', 'rkdy49')->first();
 
         $startups = [
             [
@@ -40,6 +41,32 @@ class StartupSeeder extends Seeder
                 'category' => rand(1, 3),
                 'stage' => rand(1, 3),
                 'contact' => '626abc5091',
+                'funded' => false,
+                'interests' => [1, 2, 3, 4, 5],
+                'active' => true,
+                'lookingfor' => [1, 2, 3, 4, 5],
+            ],
+            [
+                'name' => 'Brickstox',
+                'bio' => 'RealEstate ecosystem in Decentralized Finance.',
+                'about' =>
+                    'Brickstox is a blockchain powered platform where users can invest in RealEstate backed NFTs and leverage inly in the DeFi world.',
+                'logo' =>
+                    'https://cdn.discordapp.com/icons/886972630320676894/ed34537ba9f74f31750ebb2668123a6c.webp?size=240',
+                'cover' =>
+                    'https://media-exp1.licdn.com/dms/image/C4D16AQEUUuv1htmSuw/profile-displaybackgroundimage-shrink_350_1400/0/1646210107350?e=1666828800&v=beta&t=g0sKt7kXurn8PxiWldDluRVv9OY-tbM72VUz6ffVsJw',
+                'username' => 'brickstox',
+                'socials' => [
+                    'linkedin' => 'https://www.linkedin.com/company/brickstox',
+                    'instagram' => 'https://www.instagram.com/brickstox',
+                ],
+                'founded_at' => '2022-08-21',
+                'contact_email' => 'rkdy49@gmail.com',
+                'hiring_email' => 'rkdy49@gmail.com',
+                'founder' => $rkdy49->id,
+                'category' => rand(1, 3),
+                'stage' => rand(1, 3),
+                'contact' => '9479861680',
                 'funded' => false,
                 'interests' => [1, 2, 3, 4, 5],
                 'active' => true,
