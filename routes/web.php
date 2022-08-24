1d52b552b73356b0b333cad6383fde774242ba0b
meinhoonharsh <harshprogrammer782@gmail.com>
2022-08-24 23:59:13 +0530

    Formatted PHP and created edit startup route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Route;
+
 // import response
 // use Illuminate\Http\Response;
 
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     page('Home');
@@ -39,34 +40,15 @@ Route::get('startups', [AppController::class, 'startups'])->name('startups');
 Route::prefix('user')
     ->middleware('auth')
     ->group(function () {
-        Route::get('/profile/info', [ProfileController::class, 'info'])->name(
-            'profile.info'
-        );
-        Route::get('/profile/socials', [
-            ProfileController::class,
-            'socials',
-        ])->name('profile.socials');
-        Route::post('/profile/update', [
-            ProfileController::class,
-            'update',
-        ])->name('profile.update');
-        Route::post('/profile/update-cover-photo', [
-            ProfileController::class,
-            'updateCoverPhoto',
-        ])->name('profile.update.cover');
-        Route::get('/profile/refferals', [
-            ProfileController::class,
-            'refferals',
-        ])->name('profile.refferals');
-
+        Route::get('/profile/info', [ProfileController::class, 'info'])->name('profile.info');
+        Route::get('/profile/socials', [ProfileController::class, 'socials'])->name('profile.socials');
+        Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+        Route::post('/profile/update-cover-photo', [ProfileController::class, 'updateCoverPhoto'])->name('profile.update.cover');
+        Route::get('/profile/refferals', [ProfileController::class, 'refferals'])->name('profile.refferals');
         Route::prefix('startup')->group(function () {
-            Route::get('/manage', [StartupController::class, 'manage'])->name(
-                'startup.manage'
-            );
-
-            Route::post('/create', [StartupController::class, 'create'])->name(
-                'startup.create'
-            );
+            Route::get('/manage', [StartupController::class, 'manage'])->name('startup.manage');
+            Route::post('/create', [StartupController::class, 'create'])->name('startup.create');
+            Route::get('/update/{id}/info', [StartupController::class, 'updateInfo'])->name('startup.update.info');
         });
     });
 
@@ -78,26 +60,12 @@ Route::get('/testmail', [HelperController::class, 'testmail']);
 Route::view('viewmail', 'emails.test');
 
 // Social Authentication Routes
-Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name(
-    'redirectToGoogle'
-);
-Route::get('auth/google/callback', [
-    GoogleController::class,
-    'handleGoogleCallback',
-])->name('handleGoogleCallback');
-Route::get('auth/linkedin', [
-    LinkedinController::class,
-    'linkedinRedirect',
-])->name('redirectToLinkedin');
-Route::get('auth/linkedin/callback', [
-    LinkedinController::class,
-    'linkedinCallback',
-]);
-Route::get('auth/github', [GitHubController::class, 'gitRedirect'])->name(
-    'redirectToGithub'
-);
+Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('redirectToGoogle');
+Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback'])->name('handleGoogleCallback');
+Route::get('auth/linkedin', [LinkedinController::class, 'linkedinRedirect'])->name('redirectToLinkedin');
+Route::get('auth/linkedin/callback', [LinkedinController::class, 'linkedinCallback']);
+Route::get('auth/github', [GitHubController::class, 'gitRedirect'])->name('redirectToGithub');
 Route::get('auth/github/callback', [GitHubController::class, 'gitCallback']);
-
 Route::post('register', [AuthController::class, 'register'])->name('register');
 
 Route::get('/sitemap', function () {
@@ -114,7 +82,5 @@ Route::prefix('join')->group(function () {
 });
 
 Route::prefix('startup')->group(function () {
-    Route::get('/{username}', [StartupController::class, 'startup'])->name(
-        'startup'
-    );
+    Route::get('/{username}', [StartupController::class, 'startup'])->name('startup');
 });
