16f2c077310869d2e75d2da1910987a77536151d
meinhoonharsh <harshprogrammer782@gmail.com>
2022-08-26 08:34:54 +0530

    Improved Startup Model

--- app/Models/Startup.php
+++ app/Models/Startup.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use App\Models\Startup;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+
+function getUniqueStartupUsername($username)
+{
+    $startup = Startup::where('username', $username)->first();
+    if ($startup) {
+        $username = $username . rand(1, 9);
+        return getUniqueUsername($username);
+    } else {
+        return $username;
+    }
+}
 
 class Startup extends Model
 {
@@ -45,8 +57,44 @@ class Startup extends Model
     public static function boot()
     {
         parent::boot();
-        static::creating(function ($user) {
-            $user->id = (string) Str::uuid();
+        static::creating(function ($startup) {
+            $startup->id = (string) Str::uuid();
+
+            if (!$startup->username) {
+                $email = explode('@', $startup->contact_email);
+                $username = $email[0];
+
+                $startup->username = getUniqueStartupUsername($username);
+            }
+            if (!$startup->cover) {
+                // generater a random cover photo from /assets/defaults/covers
+                $startup->cover = '/assets/defaults/covers/' . rand(1, 21) . '.jpg';
+            }
+            // if (!$startup->logo) {
+            //     $startup->logo = '/assets/defaults/startuplogo.jpg';
+            // }
         });
+
     }
+
+    public function getStageAttribute($value)
+    {
+        return Stage::find($value);
+    }
+
+    public function getCategoryAttribute($value)
+    {
+        return Category::find($value);
+    }
+
+    public function getLogoAttribute($image)
+    {
+        if (!$image) {
+            return 'https://ui-avatars.com/api/?name=' .
+            urlencode($this->name) .
+                '&color=7F9CF5&background=EBF4FF';
+        }
+        return $image;
+    }
+
 }
