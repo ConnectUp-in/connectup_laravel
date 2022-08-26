b7e50dbf94b7d986d267728071cedd750161742f
meinhoonharsh <harshprogrammer782@gmail.com>
2022-08-26 10:36:23 +0530

    Created Startup 404 Page

---
+++ resources/views/pages/startup/404.blade.php
@@ -0,0 +1,131 @@
+@php
+$page['title'] = '404 - Startup Not Found | ConnectUp ';
+
+@endphp
+
+@extends('layouts/app')
+
+
+@section('content')
+    <!-- CONTENT GRID -->
+    <div class="content-grid">
+        <!-- PROFILE HEADER -->
+        <div class="profile-header v2">
+            <!-- PROFILE HEADER COVER -->
+            <figure class="profile-header-cover liquid">
+                <img src="/assets/defaults/startup404cover.png" alt="cover-29" />
+            </figure>
+            <!-- /PROFILE HEADER COVER -->
+
+            <!-- PROFILE HEADER INFO -->
+            <div class="profile-header-info">
+                <!-- USER SHORT DESCRIPTION -->
+                <div class="user-short-description big">
+                    <!-- USER SHORT DESCRIPTION AVATAR -->
+                    <a class="user-short-description-avatar user-avatar big no-stats" href="group-timeline.html">
+                        <!-- USER AVATAR BORDER -->
+                        <div class="user-avatar-border">
+                            <!-- HEXAGON -->
+                            <div class="hexagon-148-164"></div>
+                            <!-- /HEXAGON -->
+                        </div>
+                        <!-- /USER AVATAR BORDER -->
+
+                        <!-- USER AVATAR CONTENT -->
+                        <div class="user-avatar-content">
+                            <!-- HEXAGON -->
+                            <div class="hexagon-image-124-136"
+                                data-src="https://i.pinimg.com/736x/65/79/aa/6579aaaedde20287d08c896dbc8a2969.jpg"></div>
+                            <!-- /HEXAGON -->
+                        </div>
+                        <!-- /USER AVATAR CONTENT -->
+                    </a>
+                    <!-- /USER SHORT DESCRIPTION AVATAR -->
+
+                    <!-- USER SHORT DESCRIPTION AVATAR -->
+                    <a class="user-short-description-avatar user-short-description-avatar-mobile user-avatar medium no-stats"
+                        href="">
+                        <!-- USER AVATAR BORDER -->
+                        <div class="user-avatar-border">
+                            <!-- HEXAGON -->
+                            <div class="hexagon-120-130"></div>
+                            <!-- /HEXAGON -->
+                        </div>
+                        <!-- /USER AVATAR BORDER -->
+
+                        <!-- USER AVATAR CONTENT -->
+                        <div class="user-avatar-content">
+                            <!-- HEXAGON -->
+                            <div class="hexagon-image-100-110"
+                                data-src="https://i.pinimg.com/736x/65/79/aa/6579aaaedde20287d08c896dbc8a2969.jpg"></div>
+                            <!-- /HEXAGON -->
+                        </div>
+                        <!-- /USER AVATAR CONTENT -->
+                    </a>
+                    <!-- /USER SHORT DESCRIPTION AVATAR -->
+
+                    <!-- USER SHORT DESCRIPTION TITLE -->
+                    <p class="user-short-description-title">
+                        <a href="group-timeline.html">Error 404</a>
+                    </p>
+                    <!-- /USER SHORT DESCRIPTION TITLE -->
+
+                    <!-- USER SHORT DESCRIPTION TEXT -->
+                    <p class="user-short-description-text">
+                        Startup Not Found
+                    </p>
+                    <!-- /USER SHORT DESCRIPTION TEXT -->
+                </div>
+                <!-- /USER SHORT DESCRIPTION -->
+
+                <!-- USER STATS -->
+                <div class="user-stats">
+
+                    <!-- USER STAT -->
+                    <div class="user-stat big">
+                        <!-- USER STAT TITLE -->
+                        <p class="user-stat-title">4</p>
+                        <!-- /USER STAT TITLE -->
+
+                        <!-- USER STAT TEXT -->
+                        <p class="user-stat-text">Follower</p>
+                        <!-- /USER STAT TEXT -->
+                    </div>
+                    <!-- /USER STAT -->
+
+                    <!-- USER STAT -->
+                    <div class="user-stat big">
+                        <!-- USER STAT TITLE -->
+                        <p class="user-stat-title">0</p>
+                        <!-- /USER STAT TITLE -->
+
+                        <!-- USER STAT TEXT -->
+                        <p class="user-stat-text">posts</p>
+                        <!-- /USER STAT TEXT -->
+                    </div>
+                    <!-- /USER STAT -->
+
+                    <!-- USER STAT -->
+                    <div class="user-stat big">
+                        <!-- USER STAT TITLE -->
+                        <p class="user-stat-title">4</p>
+                        <!-- /USER STAT TITLE -->
+
+                        <!-- USER STAT TEXT -->
+                        <p class="user-stat-text">visits</p>
+                        <!-- /USER STAT TEXT -->
+                    </div>
+                    <!-- /USER STAT -->
+                </div>
+                <!-- /USER STATS -->
+
+
+            </div>
+            <!-- /PROFILE HEADER INFO -->
+        </div>
+        <!-- /PROFILE HEADER -->
+
+
+    </div>
+    <!-- /CONTENT GRID -->
+@endsection
