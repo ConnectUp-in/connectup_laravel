795c452695f045b20afac5907ae473870726e676
meinhoonharsh <harshprogrammer782@gmail.com>
2022-08-28 01:07:53 +0530

    Created Event MMS

---
+++ database/migrations/2022_08_27_191119_create_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            // title, slug, type, content, image, r_dates, e_dates, location, attachments, active, suspended, creator, creator_type, interests, tags, required_fields, additional_fields , created_at, updated_at
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
