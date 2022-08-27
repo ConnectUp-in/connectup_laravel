ba906358a4e49309442d8247e9ae8589ffa39bad
meinhoonharsh <harshprogrammer782@gmail.com>
2022-08-28 01:14:17 +0530

    Added Fields in event migration

--- database/migrations/2022_08_27_191119_create_events_table.php
+++ database/migrations/2022_08_27_191119_create_events_table.php
@@ -15,7 +15,25 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             // title, slug, type, content, image, r_dates, e_dates, location, attachments, active, suspended, creator, creator_type, interests, tags, required_fields, additional_fields , created_at, updated_at
-            $table->id();
+            $table->string('id');
+            $table->primary('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('type')->default('w');
+            $table->longText('content')->nullable();
+            $table->string('image')->nullable();
+            $table->json('r_dates')->nullable();
+            $table->json('e_dates')->nullable();
+            $table->string('location')->nullable();
+            $table->json('attachments')->nullable();
+            $table->boolean('active')->default(true);
+            $table->boolean('suspended')->default(false);
+            $table->string('creator')->nullable();
+            $table->string('creator_type')->nullable();
+            $table->json('interests')->nullable();
+            $table->json('tags')->nullable();
+            $table->json('required_fields')->nullable();
+            $table->json('additional_fields')->nullable();
             $table->timestamps();
         });
     }
