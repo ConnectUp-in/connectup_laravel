08121241917d2e68762352e61fed24798e2f01c4
meinhoonharsh <harshprogrammer782@gmail.com>
2022-08-28 01:42:02 +0530

    Created Event Model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,45 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+    // title, slug, type, content, image, r_dates, e_dates, location, attachments, active, event_link, suspended, creator, creator_type, interests, tags, required_fields, additional_fields , created_at, updated_at
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'type',
+        'content',
+        'image',
+        'r_dates',
+        'e_dates',
+        'location',
+        'attachments',
+        'active',
+        'suspended',
+        'creator',
+        'creator_type',
+        'interests',
+        'tags',
+        'required_fields',
+        'additional_fields',
+    ];
+
+    protected $casts = [
+        'r_dates' => 'array',
+        'e_dates' => 'array',
+        'attachments' => 'array',
+        'interests' => 'array',
+        'tags' => 'array',
+        'required_fields' => 'array',
+        'additional_fields' => 'array',
+    ];
+// If id is not present while creating a new user, then generate a random id.
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($event) {
+            if (!$event->id) {
+                $event->id = Str::uuid();
+            }
+        });
+    }
 }
