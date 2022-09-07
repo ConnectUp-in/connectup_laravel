aabaea38d540f9e0e1f2355fd43b6e51f9fffe70
meinhoonharsh <harshprogrammer782@gmail.com>
2022-09-07 20:53:51 +0530

    Fixed REgistration add Field Issue

--- resources/views/pages/event/registrations.blade.php
+++ resources/views/pages/event/registrations.blade.php
@@ -18,14 +18,14 @@
             @foreach ($event->required_fields as $field)
                 {{-- $registration->required_fields[$field] is object or array --}}
                 @if (is_array($registration->required_fields[$field]))
-                    <td>{{ $registration->required_fields[$field]['name'] }}</td>
+                    <td>{{ $registration->required_fields[$field]['name'] ?? '' }}</td>
                 @else
                     <td>{{ $registration->required_fields[$field] }}</td>
                 @endif
             @endforeach
 
             @foreach ($event->additional_fields as $field)
-                <td>{{ $registration->additional_fields[$field['identifier']] }}</td>
+                <td>{{ $registration->additional_fields[$field['identifier']] ?? '' }}</td>
             @endforeach
         </tr>
     @endforeach
