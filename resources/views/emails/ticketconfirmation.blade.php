6c59ec21a0c640910ee7669bfe88d20765fa3f64
meinhoonharsh <harshprogrammer782@gmail.com>
2022-09-10 20:57:14 +0530

    Fixed Dummy Ticket Image Load Issue

--- resources/views/emails/ticketconfirmation.blade.php
+++ resources/views/emails/ticketconfirmation.blade.php
@@ -2,18 +2,15 @@
 
 @section('content')
     <h3>Hi {{ $user->name ?? 'Entrepreneur' }},</h3>
-    <br>
 
     Your ticket for <b> <a href="{{ route('event', $event->slug) }}">{{ $event->title }}</a> </b> has been confirmed. <br>
-    <br>
-
     For Reference, your ticket id is <b>{{ $registration->ticket_id }}</b>. <br>
 
     <center>
         <h3> Here's your ticket </h3>
         <span> Bring this ticket to the event. </span>
         <br>
-        <img src="/assets/defauls/dummyticket.png" alt="ticket">
+        <img src="{{ url('/assets/defauls/dummyticket.png') }}" alt="ticket">
         <br>
         <span> Ticket is attached below. </span>
 
