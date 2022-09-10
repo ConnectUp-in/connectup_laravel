6895d02309f3dcb7aa9eed6bd4af2ff35c277891
meinhoonharsh <harshprogrammer782@gmail.com>
2022-09-10 10:00:05 +0530

    Createwd Random Ticket Generation Feature

--- app/Models/EventRegistration.php
+++ app/Models/EventRegistration.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use App\Models\EventRegistration;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+function getUniqueTicketID($id, $event_id)
+{
+    $registration = EventRegistration::where('event_id', $event_id)
+        ->where('ticket_id', $id)
+        ->first();
+    if ($registration) {
+        $id = hexdec(uniqid());
+        return getUniqueTicketID($id, $event_id);
+    } else {
+        return $id;
+    }
+
+}
 class EventRegistration extends Model
 {
     use HasFactory;
@@ -14,4 +28,15 @@ class EventRegistration extends Model
         'required_fields' => 'json',
         'additional_fields' => 'json',
     ];
+
+    // On creation of ticket_id, generate a random string
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $id = hexdec(uniqid());
+            $model->ticket_id = getUniqueTicketID($id, $model->event_id);
+        });
+    }
 }
