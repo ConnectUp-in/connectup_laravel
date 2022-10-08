e6868f3494066c424a48bffcaffc7e37de52e4be
meinhoonharsh <harshprogrammer782@gmail.com>
2022-10-08 19:59:23 +0530

    Created Members Blade Page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <title>Dashboard - ConnectUp | Connecting the Dots...</title>
-    <link rel="icon" type="image/x-icon" href="/assets/admin/src/assets/img/favicon.ico" />
     <link href="/assets/admin/layouts/collapsible-menu/css/light/loader.css" rel="stylesheet" type="text/css" />
     <link href="/assets/admin/layouts/collapsible-menu/css/dark/loader.css" rel="stylesheet" type="text/css" />
     <script src="/assets/admin/layouts/collapsible-menu/loader.js"></script>
@@ -24,6 +23,15 @@
     <link href="/assets/admin/src/assets/css/dark/dashboard/dash_1.css" rel="stylesheet" type="text/css" />
     <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
 
+    <link rel="icon" href="/assets/template/img/landing/favicon.png">
+
+    <!-- BEGIN PAGE LEVEL STYLES -->
+    <link rel="stylesheet" type="text/css" href="/assets/admin/src/plugins/src/table/datatable/datatables.css">
+
+    <link rel="stylesheet" type="text/css"
+        href="/assets/admin/src/plugins/css/light/table/datatable/dt-global_style.css">
+    <link rel="stylesheet" type="text/css"
+        href="/assets/admin/src/plugins/css/dark/table/datatable/dt-global_style.css">
 </head>
 
 <body class="layout-boxed alt-menu">
@@ -1423,16 +1431,39 @@
     <!-- END MAIN CONTAINER -->
 
     <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+    <script src="/assets/admin/src/plugins/src/global/vendors.min.js"></script>
     <script src="/assets/admin/src/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="/assets/admin/src/plugins/src/perfect-scrollbar/perfect-scrollbar.min.js"></script>
     <script src="/assets/admin/src/plugins/src/mousetrap/mousetrap.min.js"></script>
     <script src="/assets/admin/layouts/collapsible-menu/app.js"></script>
+    <script src="/assets/admin/src/assets/js/custom.js"></script>
     <!-- END GLOBAL MANDATORY SCRIPTS -->
 
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
     <script src="/assets/admin/src/plugins/src/apex/apexcharts.min.js"></script>
     <script src="/assets/admin/src/assets/js/dashboard/dash_1.js"></script>
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+    <script src="/assets/admin/src/plugins/src/table/datatable/datatables.js"></script>
+    <script>
+        $('#zero-config').DataTable({
+            "dom": "<'dt--top-section'<'row'<'col-12 col-sm-6 d-flex justify-content-sm-start justify-content-center'l><'col-12 col-sm-6 d-flex justify-content-sm-end justify-content-center mt-sm-0 mt-3'f>>>" +
+                "<'table-responsive'tr>" +
+                "<'dt--bottom-section d-sm-flex justify-content-sm-between text-center'<'dt--pages-count  mb-sm-0 mb-3'i><'dt--pagination'p>>",
+            "oLanguage": {
+                "oPaginate": {
+                    "sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>',
+                    "sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>'
+                },
+                "sInfo": "Showing page _PAGE_ of _PAGES_",
+                "sSearch": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
+                "sSearchPlaceholder": "Search...",
+                "sLengthMenu": "Results :  _MENU_",
+            },
+            "stripeClasses": [],
+            "lengthMenu": [7, 10, 20, 50],
+            "pageLength": 10
+        });
+    </script>
 
 </body>
 
