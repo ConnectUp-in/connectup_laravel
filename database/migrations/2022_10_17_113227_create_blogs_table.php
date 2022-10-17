607e22f99c1a9b778e2e5eb35e5816ea16ea277a
meinhoonharsh <harshprogrammer782@gmail.com>
2022-10-17 12:21:08 +0530

    Created Blogs Migration

---
+++ database/migrations/2022_10_17_113227_create_blogs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->string('id');
+            $table->primary('id');
+            $table->string('title');
+            $table->string('type')->default('b');
+            $table->longText('content');
+            $table->string('creator');
+            $table->string('creator_type');
+            $table->integer('category')->nullable();
+            $table->json('attachments')->nullable();
+            $table->string('slug');
+            $table->string('tags')->nullable();
+            $table->integer('readtime')->nullable();
+            $table->string('image');
+            $table->boolean('active')->default(true);
+            $table->boolean('suspended')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
