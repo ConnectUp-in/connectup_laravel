220dac22233b290c4bed5b0bc99dd12546964a46
meinhoonharsh <harshprogrammer782@gmail.com>
2022-10-18 21:53:45 +0530

    Created MMS of Follow

---
+++ database/migrations/2022_10_18_215043_create_follows_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follows', function (Blueprint $table) {
+            // user, type, followed_id, active
+            $table->id();
+            $table->string('user');
+            $table->string('type')->default('user');
+            $table->foreignId('followed_id');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follows');
+    }
+};
