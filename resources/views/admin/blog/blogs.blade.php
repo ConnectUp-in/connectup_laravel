ec8b31514a66dd85950a98dad40c1f3c2029ed87
meinhoonharsh <harshprogrammer782@gmail.com>
2022-11-04 17:29:45 +0530

    Added Action Butoons

--- resources/views/admin/blog/blogs.blade.php
+++ resources/views/admin/blog/blogs.blade.php
@@ -70,7 +70,26 @@
                                     </td>
                                     <td>{{ $blog->updated_at->diffForHumans() }}</td>
                                     <td>
+                                        <div class="dropdown">
+                                            <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink20"
+                                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="feather feather-more-horizontal">
+                                                    <circle cx="12" cy="12" r="1"></circle>
+                                                    <circle cx="19" cy="12" r="1"></circle>
+                                                    <circle cx="5" cy="12" r="1"></circle>
+                                                </svg>
+                                            </a>
 
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink20">
+                                                <a class="dropdown-item" href="{{ route('blog', $blog->slug) }}">View</a>
+                                                <a class="dropdown-item"
+                                                    href="{{ route('superadmin.blog.edit', $blog->id) }}">Edit</a>
+                                                <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                                            </div>
+                                        </div>
                                     </td>
                                 </tr>
 
