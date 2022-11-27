0b59c19767e8d23c7b87118a233e50ffbd668933
meinhoonharsh <harshprogrammer782@gmail.com>
2022-11-27 20:32:02 +0530

    Changed WA join to Network 02

--- routes/web.php
+++ routes/web.php
@@ -88,7 +88,7 @@ Route::middleware('sitemap')->group(function () {
         Route::redirect('team/design', 'https://forms.gle/4TFTJFzavqt8jH9z5', 301);
         Route::redirect('team/dev', 'https://forms.gle/qbYC4dxfx6MkMbZW7', 301);
         Route::redirect('team/operations', 'https://forms.gle/9UqAh9yyuwN2gfFZA', 301);
-        Route::redirect('wa', 'https://chat.whatsapp.com/EON6L3i0HNbL3dKwdUYd63', 301);
+        Route::redirect('wa', 'https://chat.whatsapp.com/LjiYejqWEnVHGaPvQDzMLW', 301);
         Route::redirect('team', 'https://forms.gle/CUHvK4y6bgPcBm2LA', 301);
         Route::redirect('ecell', 'https://ru1pr7e2uk9.typeform.com/to/iUdWLktz', 301);
 
