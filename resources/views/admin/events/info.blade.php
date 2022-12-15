c655b361100374cf7b19e72018bd710c31e10866
Shivansh Gupta <95837362+shivanshg06@users.noreply.github.com>
2022-12-15 16:01:55 +0530

    Fixed Errors on event info screen

--- resources/views/admin/events/info.blade.php
+++ resources/views/admin/events/info.blade.php
@@ -89,7 +89,6 @@
                                     <th>User</th>
                                     <th>Registered At</th>
                                     <th>Agent</th>
-
                                 </tr>
                             </thead>
                             <tbody>
@@ -104,11 +103,11 @@
                                                 <p class="align-self-center mb-0 admin-name">
                                                     {{ $registree->user->name }}
                                                 </p>
-
                                             </div>
                                         </td>
                                         <td> {{ Carbon::parse($registree->created_at)->diffForHumans() }} </td>
                                         @php
+                                            require 'vendor/autoload.php';
                                             $result = new \WhichBrowser\Parser($registree->agent);
                                         @endphp
                                         <td> {{ $result->toString() }} </td>
@@ -156,20 +155,11 @@
                                         <td> {{ $registree->email }} </td>
                                         <td> {{ $registree->created_at }} </td>
                                         <td> {{ $registree->ticket_id }} </td>
-                                        @if (count($event->required_fields) != 0)
-                                            <td>
-                                                @foreach ($registree->required_fields as $r_fields)
-                                                    {{ $reason['name'] }}
-                                                    @if (!$loop->last)
-                                                        ,
-                                                    @endif
-                                                @endforeach
-                                            </td>
-                                        @endif
+                                        <td> {{ $registree->required_fields }} </td>
+                                        <td> {{ $registree->additional_fields }} </td>
                                     </tr>
                                 @empty
                                 @endforelse
-
                             </tbody>
                         </table>
                     </div>
