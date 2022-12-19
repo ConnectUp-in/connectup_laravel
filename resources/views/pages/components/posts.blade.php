bc38ba1acc5d3842c0edfd6d62edd3e6edd99d84
meinhoonharsh <harshprogrammer782@gmail.com>
2022-12-19 20:48:47 +0530

    Added Infinite Feed Feature

--- resources/views/pages/components/posts.blade.php
+++ resources/views/pages/components/posts.blade.php
@@ -1,11 +1,18 @@
-{{-- <div class="posts-container"></div> --}}
+<div class="posts-container" style="display: flex;flex-direction:column;gap:16px"></div>
 
-@forelse ($posts as $post)
+<div class="no-data">
+    Great! You have read all the posts. <br>
+    <a href="{{ route('feed') }}">Refresh</a>
+</div>
+
+{{-- @forelse ($posts as $post)
     @import('post', ['post' => $post])
 @empty
-@endforelse
+@endforelse --}}
 
 @push('scripts')
+    {{-- Import  momenthjs --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
     <script>
         function getPostHTMLbyJSON(post) {
             var tags, ytlink, meta, html = '';
@@ -150,19 +157,26 @@
                         <span class="bold">post</span>
                     </p>
                     <p class="user-status-text small">
-                        ${post.created_at}
+                        ${moment(post.created_at).fromNow()}
                     </p>
                 </div>
                 <p class="widget-box-status-text">${post.caption }</p>
             </div>
 
-            ${meta}
-            ${ytlink}
+            ${meta ?? ''}
+            ${ytlink ?? ''}
 
 
 
                 ${tags}
+
+
+
         </div>
+
+        ${postoptions ?? ''}
+
+
     </div>`
 
             return html;
@@ -172,15 +186,207 @@
 
 
         }
-        // $(document).ready(function() {
-        //     console.log('sending request');
-        //     $.get('{{ route('getposts') }}', function(data) {
-        //         console.log(data);
-        //         $.each(data, function(index, value) {
-        //             console.log(value);
-        //             $('.posts-container').append(getPostHTMLbyJSON(value));
-        //         });
-        //     });
-        // });
+        $(document).ready(function() {
+            console.log('sending request');
+            $.get('{{ route('getposts') }}', function(data) {
+                console.log(data);
+                $.each(data, function(index, value) {
+                    console.log(value);
+                    $('.posts-container').append(getPostHTMLbyJSON(value));
+                });
+
+                updateUI();
+
+
+            });
+        });
+
+
+        function updateUI() {
+
+            app.plugins.createHexagon({
+                container: '.hexagon-image-30-32',
+                width: 30,
+                height: 32,
+                roundedCorners: true,
+                roundedCornerRadius: 1,
+                clip: true
+            });
+
+
+            app.plugins.createHexagon({
+                container: '.hexagon-progress-40-44',
+                width: 40,
+                height: 44,
+                lineWidth: 3,
+                roundedCorners: true,
+                roundedCornerRadius: 1,
+                gradient: {
+                    colors: ['#d9ff65', '#40d04f']
+                },
+                scale: {
+                    start: 0,
+                    end: 1,
+                    stop: .8
+                }
+            });
+
+
+            app.plugins.createHexagon({
+                container: '.hexagon-border-40-44',
+                width: 40,
+                height: 44,
+                lineWidth: 3,
+                roundedCorners: true,
+                roundedCornerRadius: 1,
+                lineColor: '#293249'
+            });
+
+            app.plugins.createDropdown({
+                trigger: ".widget-box-post-settings-dropdown-trigger",
+                container: ".widget-box-post-settings-dropdown",
+                offset: {
+                    top: 30,
+                    right: 9,
+                },
+                animation: {
+                    type: "translate-top",
+                    speed: 0.3,
+                    translateOffset: {
+                        vertical: 20,
+                    },
+                },
+            });
+
+
+            app.plugins.createDropdown({
+                trigger: ".reaction-options-dropdown-trigger",
+                container: ".reaction-options-dropdown",
+                triggerEvent: "click",
+                offset: {
+                    bottom: 54,
+                    left: -16,
+                },
+                animation: {
+                    type: "translate-bottom",
+                    speed: 0.3,
+                    translateOffset: {
+                        vertical: 20,
+                    },
+                },
+                closeOnDropdownClick: true,
+            });
+
+            app.plugins.createDropdown({
+                trigger: ".share-options-dropdown-trigger",
+                container: ".share-options-dropdown",
+                triggerEvent: "click",
+                offset: {
+                    bottom: 54,
+                    right: -16,
+                },
+                animation: {
+                    type: "translate-bottom",
+                    speed: 0.3,
+                    translateOffset: {
+                        vertical: 20,
+                    },
+                },
+                closeOnDropdownClick: true,
+            });
+        }
+    </script>
+
+
+    <script>
+        let pages = 2;
+        let current_page = 0;
+        let bool = false;
+        let lastPage;
+        $(window).scroll(function() {
+            let height = $(document).height();
+
+            let containerbottom = $(".posts-container").offset().top + $(".posts-container").height()
+
+            if (($(window).scrollTop() + $(window).height()) >= containerbottom && bool == false) {
+                bool = true;
+                $('.ajax-load').show();
+
+                lazyLoad(pages)
+                    .then(() => {
+                        bool = false;
+                        pages++;
+                        if (pages - 2 == lastPage) {
+                            bool = true;
+                            $('.no-data').show();
+                        }
+                    })
+            }
+        })
+
+        function lazyLoad(page) {
+            return new Promise((resolve, reject) => {
+                $.ajax({
+                    url: '?page=' + page,
+                    type: 'GET',
+                    beforeSend: function() {
+                        $('.ajax-load').show();
+                    },
+                    success: function(response) {
+                        $('.ajax-load').hide();
+                        console.log(response);
+                        let html = '';
+                        for (let i = 0; i < response.data.data.length; i++) {
+                            html += getPostHTMLbyJSON(response.data.data[i]);
+                        }
+                        $('.posts-container').append(html);
+                        lastPage = response.data.last_page;
+                        updateUI();
+                        resolve();
+                    }
+                });
+            })
+        }
+        // loadData(1);
+        // function loadData(page){
+        //     $.ajax({
+        //        url: '?page='+page,
+        //        type:'GET',
+        //        beforeSend:function() {
+        //          $('.ajax-load').show();
+        //        },
+        //        success :function (response){
+        //            $('.ajax-load').hide();
+        //            lastPage = response.data.last_page;
+        //            console.log(response);
+        //           let html = '';
+        //         for(let i = 0; i < response.data.data.length;i++){
+        //             html += `
+    //               <div class="col-md-4 mb-3" >
+    //                 <div class="card">
+    //                     <div class="card-header">
+    //                     Employee Title
+    //                     </div>
+    //    <div class="card-body">
+    //         <table class="table">
+    //            <tr>
+    //                <th>Name</th>
+    //                 <td>:</td>
+    //                 <td>`+response.data.data[i].name+`</td>
+    //              </tr>
+    //              <tr>
+    //               <th>Phone</th>
+    //                 <td>:</td>
+    //                <td>`+response.data.data[i].phone+`</td>
+    //               </tr>
+    //          </table>
+    //      </div>
+    //      </div>
+    // </div>`;
+        //       }
+        //       $('#data_temp').html(html);
+        //    }
+        //   });
+        // }
     </script>
 @endpush
