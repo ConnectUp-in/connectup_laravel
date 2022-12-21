92b522e2c6ed954839669237c5798b09a18ba1ef
meinhoonharsh <harshprogrammer782@gmail.com>
2022-12-21 13:47:07 +0530

    Fixed Security Vulnerability Web Vitals

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -233,7 +233,7 @@
 
 
     {{-- import jquery from cdn --}}
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
     <!-- app -->
