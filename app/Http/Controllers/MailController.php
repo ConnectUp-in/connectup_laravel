a2ddb6a0acc4864dd149e106e38e7490e0500a7d
meinhoonharsh <harshprogrammer782@gmail.com>
2023-01-01 01:45:25 +0530

    Addeed Exam Update Email

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -21,16 +21,8 @@ class MailController extends Controller
                 "name": "Shah Alam"
             },
             {
-                "email": "kanakvishwakarma000@gmail.com",
-                "name": "Kanak Vishwakarma"
-            },
-            {
                 "email": "piyushpjani@gmail.com",
                 "name": "PIYUSH JANI"
-            },
-            {
-                "email": "kachamsatwika@gmail.com",
-                "name": "kacham satwika"
             },
             {
                 "email": "spayasi78@gmail.com",
@@ -43,10 +35,6 @@ class MailController extends Controller
             {
                 "email": "kapiljadon5849@gmail.com",
                 "name": "Kapil Jadon"
-            },
-            {
-                "email": "prempandey812743@gmail.com",
-                "name": "Prem Prakash Pandey"
             },
             {
                 "email": "rrsaggarwal@gmail.com",
@@ -67,10 +55,6 @@ class MailController extends Controller
             {
                 "email": "guptasurajkumar577@gmail.com",
                 "name": "Suraj Kumar"
-            },
-            {
-                "email": "arjundhilip38@gmail.com",
-                "name": "S B Arjun Dhilip "
             },
             {
                 "email": "fs20if022@gmail.com",
@@ -117,16 +101,8 @@ class MailController extends Controller
                 "name": "Utkarsh Arjariya"
             },
             {
-                "email": "sunilkumar088839@gmail.com",
-                "name": "Sunil Kumar"
-            },
-            {
                 "email": "emmanuelesin950@gmail.com",
                 "name": "Esin Emmanuel William "
-            },
-            {
-                "email": "pawarankit.jec@gmail.com",
-                "name": "Ankit Pawar"
             },
             {
                 "email": "prathamakkewar7@gmail.com",
@@ -154,13 +130,14 @@ class MailController extends Controller
                 'email' => $user['email'],
             ];
             // $data = [];
-            Mail::send('emails.taskreminder', $data, function ($message) use ($data) {
+            Mail::send('emails.hiring.dev.examsupdate', $data, function ($message) use ($data) {
                 $message->from('connectup.in@gmail.com', 'Team ConnectUp');
                 $message->to($data['email'], $data['name']);
                 // $message->subject("Time to shine: Complete Round II | Web Developer @ConnectUp");
                 // $message->subject("Congratulations!!! Your application has been shortlisted for Round-I | Web Developer @ConnectUp");
                 // $message->subject("[Action Required] Complete the Round-1 Test by deadline");
-                $message->subject(" [Reminder] Clock is Ticking: Submit your Web Development Round-2 Task ");
+                // $message->subject(" [Reminder] Clock is Ticking: Submit your Web Development Round-2 Task ");
+                $message->subject(" [Update] Flexible deadline for Round - II | Web Developer @ConnectUp");
 
             });
 
