d5b1bb8754710314217764fc6cf2ad90fdda9580
meinhoonharsh <harshprogrammer782@gmail.com>
2023-01-01 03:37:40 +0530

    Added Design hiring mail Templates

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -17,96 +17,176 @@ class MailController extends Controller
     {
         $usersjson = '[
             {
-                "email": "alamshah1617@gmail.com",
-                "name": "Shah Alam"
+                "email": "officialkowsalya@gmail.com",
+                "name": "Kowsalya"
             },
             {
-                "email": "piyushpjani@gmail.com",
-                "name": "PIYUSH JANI"
+                "email": "anirudh0502@gmail.com",
+                "name": "Anirudh Aggarwal"
             },
             {
-                "email": "spayasi78@gmail.com",
-                "name": "Santosh kumar Payasi"
+                "email": "sheikziya21@gmail.com",
+                "name": "Fathima "
             },
             {
-                "email": "loveguptaofficials@gmail.com",
-                "name": "love gupta"
+                "email": "pranitamagarde2000@gmail.com",
+                "name": "Pranita Magarde"
             },
             {
-                "email": "kapiljadon5849@gmail.com",
-                "name": "Kapil Jadon"
+                "email": "anknalitoriya@gmail.com",
+                "name": "Ankna Litoriya"
             },
             {
-                "email": "rrsaggarwal@gmail.com",
-                "name": "Roshni Aggarwal"
+                "email": "kathal.anushka@gmail.com",
+                "name": "Anushka Kathal "
             },
             {
-                "email": "sonamaafremah@gmail.com",
-                "name": "Sonam Aafre"
+                "email": "mrbhupeshsharmaa@gmail.com",
+                "name": "bhupesh sharma"
             },
             {
-                "email": "anushkayadav7987@gmail.com",
-                "name": "Anushka yadav "
+                "email": "aviaruna08@gmail.com",
+                "name": "BHUVAN S BIRADAR"
             },
             {
-                "email": "kongarapiaashamadhuri@gmail.com",
-                "name": "K.Aasha madhuri"
+                "email": "chaturvedigourav675@gmail.com",
+                "name": "Gourav kumar chaturvedi"
             },
             {
-                "email": "guptasurajkumar577@gmail.com",
-                "name": "Suraj Kumar"
+                "email": "kanikagupta30398@gmail.comcom",
+                "name": "Kanika gupta"
             },
             {
-                "email": "fs20if022@gmail.com",
-                "name": "Dhruv Pralhad Paste"
+                "email": "tntmarketing2018@gmail.com",
+                "name": "Avdesh Patidar"
             },
             {
-                "email": "ashwinihinge6666@gmail.com",
-                "name": "Ashwini hinge"
+                "email": "mu4013314@gmail.com",
+                "name": "Mukesh Kumar uikey "
             },
             {
-                "email": "jitendraaakde8959@gmail.com",
-                "name": "Jitendra Aakde "
+                "email": "clashroya4613@gmail.com",
+                "name": "Mehtab Khan"
             },
             {
-                "email": "akshatmishra782@gmail.com",
-                "name": "Akshat Mishra "
+                "email": "anjaliupadhyay1866@gmail.com",
+                "name": "Anjali Upadhyay"
             },
             {
-                "email": "saharshsaxena007@gmail.com",
-                "name": "Saharsh Saxena"
+                "email": "deepak512panwar@gmail.com",
+                "name": "Deepak Panwar "
             },
             {
-                "email": "ravirungta51@gmail.com",
-                "name": "Ravi Kumar Singh"
+                "email": "satyambaniya789@gmail.com",
+                "name": "Satyam Baniya"
             },
             {
-                "email": "pbansod96@gmail.com",
-                "name": "AMAN NARENDRA BANSOD"
+                "email": "akashk12516@gmail.com",
+                "name": "Akash kumar "
             },
             {
-                "email": "tanmaywaykar17@gmail.com",
-                "name": "Tanmay Waykar"
+                "email": "ashonak@gmail.com",
+                "name": "SHASHANK SHONAK"
             },
             {
-                "email": "pathraberavishakar@gmail.com",
-                "name": "Saurabh pathrabe "
+                "email": "zaheershaik97042@gmail.com",
+                "name": "Shaik zaheer"
             },
             {
-                "email": "amansinghkushwaha2003@gmail.com",
-                "name": "Aman Kumar Singh Kushwaha"
+                "email": "hariprakash9219@gmail.com",
+                "name": "HARI PRAKASH"
             },
             {
-                "email": "utkarsharjariyaofficial@gmail.com",
-                "name": "Utkarsh Arjariya"
+                "email": "payalmanghnani11@gmail.com",
+                "name": "Payal Manghnani"
             },
             {
-                "email": "emmanuelesin950@gmail.com",
-                "name": "Esin Emmanuel William "
+                "email": "kkabdwal178@gmail.com",
+                "name": "Kartik "
+            },
+            {
+                "email": "chinnagarianil@gmail.com",
+                "name": "Chinnagari Anil"
+            },
+            {
+                "email": "devagrawal310@gmail.com",
+                "name": "Dev Agrawal"
+            },
+            {
+                "email": "mohitpardhi58@gmail.com",
+                "name": "Mohit "
             },
             {
                 "email": "prathamakkewar7@gmail.com",
-                "name": "Pratham Manoj Akkewar"
+                "name": "Pratham Akkewar"
+            },
+            {
+                "email": "shasvat007mishra@gmail.com",
+                "name": "Shasvat Mishra "
+            },
+            {
+                "email": "harshitb2006@gmail.com",
+                "name": "Harshit Bhandari"
+            },
+            {
+                "email": "alpmuk2001@gmail.com",
+                "name": "Yogita Singh"
+            },
+            {
+                "email": "moharanasidheswar2001@gmail.com",
+                "name": "Sidheswar Moharana"
+            },
+            {
+                "email": "gab2021147@sicsr.ac.in",
+                "name": "Gautam Bagra"
+            },
+            {
+                "email": "ayushkumar2205@gmail.com",
+                "name": "Ayush Kumar"
+            },
+            {
+                "email": "soultoluene@gmail.com",
+                "name": "Johnson Tolulope"
+            },
+            {
+                "email": "arjundhilip38@gmail.com",
+                "name": "S B Arjun Dhilip"
+            },
+            {
+                "email": "segunadeogundesigns@gmail.com",
+                "name": "Segun Adeogun"
+            },
+            {
+                "email": "onwudiweebube0@gmail.com",
+                "name": "Ebubechukwu Onwudiwe "
+            },
+            {
+                "email": "joshuadiran2000@gmail.com",
+                "name": "Oluwadiran Joshua Ireoluwa "
+            },
+            {
+                "email": "onyewuchidaniel6@gmail.com",
+                "name": "Onyewuchi Daniel"
+            },
+            {
+                "email": "balogun1069@gmail.com",
+                "name": "Abdulazeez Balogun"
+            },
+            {
+                "email": "mustofaalameen54@gmail.com",
+                "name": "Mustofa Al Ameen Mustafa"
+            },
+            {
+                "email": "berniceyydesign@gmail.com",
+                "name": "Oparah-ndu Chinaza Bernice "
+            },
+            {
+                "email": "adeola19980@gmail.com",
+                "name": "Opeyemi Adeola"
+            },
+            {
+                "email": "dynamic1sam@gmail.com",
+                "name": "Samson Sokoti Oluwabunmi"
             },
             {
                 "email": "harshprogrammer782@gmail.com",
@@ -130,14 +210,14 @@ class MailController extends Controller
                 'email' => $user['email'],
             ];
             // $data = [];
-            Mail::send('emails.hiring.dev.examsupdate', $data, function ($message) use ($data) {
+            Mail::send('emails.hiring.design.task', $data, function ($message) use ($data) {
                 $message->from('connectup.in@gmail.com', 'Team ConnectUp');
                 $message->to($data['email'], $data['name']);
                 // $message->subject("Time to shine: Complete Round II | Web Developer @ConnectUp");
-                // $message->subject("Congratulations!!! Your application has been shortlisted for Round-I | Web Developer @ConnectUp");
+                $message->subject("Congratulations!!! Your application has been shortlisted for Round-I | Graphic Designer @ConnectUp");
                 // $message->subject("[Action Required] Complete the Round-1 Test by deadline");
                 // $message->subject(" [Reminder] Clock is Ticking: Submit your Web Development Round-2 Task ");
-                $message->subject(" [Update] Flexible deadline for Round - II | Web Developer @ConnectUp");
+                // $message->subject(" [Update] Flexible deadline for Round - II | Web Developer @ConnectUp");
 
             });
 
