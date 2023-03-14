72e9cc98fe8c97d817cd3443db8e28c68282cd8d
meinhoonharsh <harshprogrammer782@gmail.com>
2023-03-14 16:29:29 +0530

    Fixed Codebase

--- resources/views/pages/blog/blog.blade.php
+++ resources/views/pages/blog/blog.blade.php
@@ -4,236 +4,6 @@
     $page['image'] = $blog->image ?? '';
     
 @endphp
-{{-- 
-@extends('layouts.guest')
-
-
-@section('content')
-    <!-- BANNER WRAP -->
-    <div id="top" class="banner-wrap">
-        <!-- BANNER -->
-        <div class="banner grid-limit">
-            <!-- BANNER INFO -->
-            <div class="banner-info">
-
-                <!-- BANNER PRETITLE -->
-                <p class="banner-pretitle">
-                    Last Updated: &nbsp;{{ $blog->updated_at->diffForHumans() }}
-                </p>
-                <!-- /BANNER PRETITLE -->
-
-                <!-- BANNER TITLE -->
-                <p class="banner-title">{{ $blog->title }}</p>
-                <!-- /BANNER TITLE -->
-
-                <!-- BANNER TEXT -->
-                <p class="banner-text">
-
-                    {{ $blog->readtime }} min read &nbsp;|&nbsp; {{ changeIntoKMG(profileview($blog->id)) }} Views
-
-                </p>
-                <!-- /BANNER TEXT -->
-
-            </div>
-            <!-- /BANNER INFO -->
-        </div>
-        <!-- /BANNER -->
-    </div>
-    <!-- /BANNER WRAP -->
-
-
-
-
-    <!-- SECTION WRAP -->
-    <div class="section-wrap  dark" id="about">
-
-        <div class="container row">
-            <div class="main-container col-md-9">
-                <div class="cover-container">
-                    <img src="{{ $blog->image }}" alt="cover" class="cover-img" onerror="this.display='none'">
-                </div>
-
-                @ad('display-horizontal')
-
-                <div class="blog-content">
-                    {!! $blog->content !!}
-                </div>
-
-                <div class="category-holder">
-                    <b>Category:</b>
-                    <span class="category-name">
-                        <a href="#">
-                            {{ $blog->category->name ?? 'No Category selected' }}
-                        </a>
-                    </span>
-                </div>
-                <!-- TAG LIST -->
-                <div class="tag-holder">
-                    @foreach ($blog->tags as $tag)
-                        <a class="tag" href="#">{{ $tag }}</a>
-                    @endforeach
-                </div>
-                <!-- /TAG LIST -->
-            </div>
-            <div class="side-container col-md-3">
-                @ad('display-square')
-
-            </div>
-        </div>
-
-    </div>
-    <!-- /SECTION WRAP -->
-@endsection
-
-
-@section('styles')
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@400;500;600&display=swap" rel="stylesheet">
-
-    <style>
-        .banner-wrap {
-            background-image: url(https://cdn.wallpapersafari.com/31/2/A7C1GQ.jpg);
-        }
-
-        .banner {
-            min-height: 30vh;
-        }
-
-        .banner .banner-info {
-            padding: 10vh 5vh;
-            padding-top: 15vh;
-        }
-
-
-        .banner .banner-pretitle {
-            text-transform: none;
-        }
-
-        .banner .banner-title {
-            font-size: 4rem;
-            text-align: center;
-            text-transform: inherit;
-            font-family: 'Mitr', sans-serif;
-            text-shadow: 0 0 50px #0009;
-            margin-top: 0.3em;
-        }
-
-        .section-wrap {
-            padding: 3em;
-        }
-
-        .container {
-            width: 90%;
-            margin: 0 auto;
-            display: flex;
-        }
-
-        .main-container {
-            width: 70%;
-        }
-
-        .side-container {
-            width: 30%;
-        }
-
-        .cover-container {
-            width: 100%;
-            overflow: hidden;
-            border-radius: 10px;
-            margin-bottom: 1em;
-        }
-
-        .cover-img {
-            width: 100%;
-            object-fit: cover;
-        }
-
-        .blog-content {
-            color: #fff !important;
-            font-size: 1.4em;
-        }
-
-        .blog-content>* {
-            color: #b6b7ce !important;
-        }
-
-
-        .blog-content p {
-            margin-bottom: 0.1em;
-            line-height: 1.5;
-        }
-
-        .category-holder {
-            margin-top: 1em 0;
-            font-family: Rajdhani, sans-serif;
-            color: #b6b7ce;
-            font-size: 1.2em;
-        }
-
-
-        .category-name a {
-            color: #fff;
-            text-decoration: none;
-            font-weight: 500
-        }
-
-        .tag-holder {
-            margin-top: 1em;
-            margin-bottom: 1em;
-        }
-
-        .tag {
-            display: inline-block;
-            margin-right: 0.5em;
-            margin-bottom: 0.5em;
-            padding: 0.5em 1em;
-            background-color: #23D2E2;
-            border-radius: 10px;
-            color: #0d1117;
-            text-decoration: none;
-            font-weight: 500;
-            transition: 0.2s ease;
-        }
-
-        .tag:hover {
-            cursor: pointer;
-            transform: translateY(-3px);
-        }
-
-        @media screen and (max-width: 768px) {
-
-            .section-wrap {
-                padding: 2em 0;
-            }
-
-            .banner .banner-info {
-                padding: 10vh 0;
-                padding-top: 15vh;
-            }
-
-            .banner .banner-title {
-                font-size: 2.5em;
-            }
-
-            .container {
-                flex-direction: column;
-            }
-
-            .main-container {
-                width: 100%;
-            }
-
-
-
-            .side-container {
-                width: 100%;
-            }
-
-        }
-    </style>
-@endsection --}}
-
 
 @extends('layouts.home')
 
@@ -748,20 +518,20 @@
                     <div class="widget">
                         <h4 class="widget-title mb-3">Recent Blogs</h4>
                         <ul class="image-list">
-                            @foreach (getRecentBlogs(3) as $blog)
+                            @foreach (getRecentBlogs(3) as $b)
                                 <li>
-                                    <figure class="rounded"><a href="{{ route('blog', $blog->slug) }}"><img
-                                                src="{{ $blog->image_compressed }}"
-                                                onerror="this.src='{{ $blog->image }}'" alt="" /></a></figure>
+                                    <figure class="rounded"><a href="{{ route('blog', $b->slug) }}"><img
+                                                src="{{ $b->image_compressed }}" onerror="this.src='{{ $b->image }}'"
+                                                alt="" /></a></figure>
                                     <div class="post-content">
-                                        <h6 class="mb-1"> <a class="link-dark" href="{{ route('blog', $blog->slug) }}">
-                                                {{ Str::limit($blog->title, 30) }}</a> </h6>
+                                        <h6 class="mb-1"> <a class="link-dark" href="{{ route('blog', $b->slug) }}">
+                                                {{ Str::limit($b->title, 30) }}</a> </h6>
                                         <ul class="post-meta">
                                             <li class="post-date"><i
-                                                    class="uil uil-calendar-alt"></i><span>{{ $blog->updated_at->diffForHumans() }}</span>
+                                                    class="uil uil-calendar-alt"></i><span>{{ $b->updated_at->diffForHumans() }}</span>
                                             </li>
                                             <li class="post-comments"><a href="#"><i
-                                                        class="uil uil-eye"></i>{{ changeIntoKMG(profileview($blog->id)) }}</a>
+                                                        class="uil uil-eye"></i>{{ changeIntoKMG(profileview($b->id)) }}</a>
                                             </li>
                                         </ul>
                                         <!-- /.post-meta -->
